a0f32a96df8e57f41835a126133cccf2a4392be6
JunKyu Choi <122720798+junkyn@users.noreply.github.com>
2023-12-06 03:30:43 +0900

    Update RVC7.c

--- RVC7.c
+++ RVC7.c
@@ -28,43 +28,12 @@ int main(){
 	_beginthreadex(NULL,0,CleanerControl,0,0,NULL);
 	while(1){
 		if(MoveBackward){
-			if(Obstacle[0] && Obstacle[1]) MoveBackward = 1;
-			else if(!Obstacle[0]){
-				MoveBackward = 0;
-				do{
-					TurnLeft();
-				}while(Obstacle[2]);
-			}else if(Obstacle[0] && !Obstacle[1]){
-				MoveBackward = 0;
-				do{
-					TurnRight();
-				}while(Obstacle[2]);
-			}
+			CtrlMoveBackward();
 		}	
 		else{
-			if(!Obstacle[2]) {
-				MoveForward = 1;
-				if(Dust) CleanerCommand = 2;
-				else CleanerCommand = 1;
-			}
-			else if(Obstacle[2] && !Obstacle[0]){
-				MoveForward = 0;
-				CleanerCommand = 0;
-				TurnLeft();
-			}else if(Obstacle[0] && !Obstacle[1] && Obstacle[2]){
-				MoveForward = 0;
-				CleanerCommand = 0;
-				TurnRight();
-			}else if(Obstacle[0] && Obstacle[1] && Obstacle[2]){
-				MoveForward = 0;
-				CleanerCommand = 0;
-				MoveBackward = 1;
-			}
+			CtrlMoveNotBackward();
 		}
-		
-
 		Sleep(10);
-		
 	}
 }
 
@@ -105,6 +74,40 @@ unsigned _stdcall CleanerControl(void* arg){
 		Sleep(10);
 	}
 }
+void CtrlMoveBackward(){
+		if(Obstacle[0] && Obstacle[1]) MoveBackward = 1;
+		else if(!Obstacle[0]){
+			MoveBackward = 0;
+			do{
+				TurnLeft();
+			}while(Obstacle[2]);
+		}else if(Obstacle[0] && !Obstacle[1]){
+			MoveBackward = 0;
+			do{
+				TurnRight();
+			}while(Obstacle[2]);
+		}
+}
+void CtrlMoveNotBackward(){
+		if(!Obstacle[2]) {
+			MoveForward = 1;
+			if(Dust) CleanerCommand = 2;
+			else CleanerCommand = 1;
+		}
+		else if(Obstacle[2] && !Obstacle[0]){
+			MoveForward = 0;
+			CleanerCommand = 0;
+			TurnLeft();
+		}else if(Obstacle[0] && !Obstacle[1] && Obstacle[2]){
+			MoveForward = 0;
+			CleanerCommand = 0;
+			TurnRight();
+		}else if(Obstacle[0] && Obstacle[1] && Obstacle[2]){
+			MoveForward = 0;
+			CleanerCommand = 0;
+			MoveBackward = 1;
+		}
+}
 void SetCleaner(int Level){
 	//Level이 0이면 흡입 중단, 1이면 흡입력 중간으로 흡입, 2이면 흡입력 강하게 흡입
 }
@@ -119,16 +122,20 @@ void RightMotorControl(int dir){
 	//오른쪽 모터를 dir이 0이면 전진, dir이 1이면 후진
 }
 int LeftSensor(){
-	//RVC의 왼쪽에 달려있는 센서가 장애물을 감지하면 1을 return, 감지하지 못하면 0을 return
+	//RVC의 왼쪽에 달려있는 센서가 장애물을 감지하면 1을 return, 감지하지 못하면 0을 return, 오류 시 -1을 return
+	return -1;
 }
 int RightSensor(){
-	//RVC의 오른쪽에 달려있는 센서가 장애물을 감지하면 1을 return, 감지하지 못하면 0을 return
+	//RVC의 오른쪽에 달려있는 센서가 장애물을 감지하면 1을 return, 감지하지 못하면 0을 return, 오류 시 -1을 return
+	return -1;
 }
 int FrontSensor(){
-	//RVC의 앞쪽에 달려있는 센서가 장애물을 감지하면 1을 return, 감지하지 못하면 0을 return
+	//RVC의 앞쪽에 달려있는 센서가 장애물을 감지하면 1을 return, 감지하지 못하면 0을 return, 오류 시 -1을 return
+	return -1;
 }
 int isDust(){
-	//RVC의 먼지 감지 센서가 먼지를 감지하면 1을 return, 감지하지 못하면 0을 return
+	//RVC의 먼지 감지 센서가 장애물을 감지하면 1을 return, 감지하지 못하면 0을 return, 오류 시 -1을 return
+	return -1;
 }
 void TurnLeft(){
 	for(int i = 0; i<5; i++){
